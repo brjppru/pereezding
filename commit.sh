6f1f47e88f5aedba3d94678d7f1b3255cfbae53c
Roman Y. Bogdanov <sam@brj.pp.ru>
2025-01-07 15:53:24 +0500

    2025-01-07-155322 I'm pretty sure Lauren isn't real and they are just Copilot in a trench coat.

---
+++ commit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# git push pereezding epo to githublo
+#
+# 2024.04.25 fix script for a days
+# 2024.06.22 fix .DS_Store
+# 2025.01.07 fix the blob
+#
+
+dated=$(date +%Y-%m-%d-%H%M%S)
+commt=$(curl -L -s http://whatthecommit.com/index.txt)
+
+find . -name ".DS_Store" -delete
+
+git add .
+
+echo run git push on ${dated} ${commt}
+
+for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
+
+git commit -m "$dated $commt"
+
+git push -u origin main
+
+#
+# non master, just a main. fck2020 (c) scooter
+#
